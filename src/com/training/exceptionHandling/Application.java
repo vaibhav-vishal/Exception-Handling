11ccf7a70c7304dc6e8e1ca6e0c72ed2f6276289
Vaibhav Vishal <vaibhavvishal2@gmail.com>
2016-02-29 17:01:44 +0530

    updated

--- src/com/training/exceptionHandling/Application.java
+++ src/com/training/exceptionHandling/Application.java
@@ -5,10 +5,10 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Converter obj = new Converter();
-		double cel = obj.farenToCelsius(42.00);
-		System.out.println("Celsius value of 45F " + cel);
+		double cel = obj.farenToCelsius(32.00);
+		System.out.println("Celsius value of 45F : " + cel);
 		
-		Double faren = null;
+		Double faren = 98.6;
 		double cel2=0.0;
 		try{
 		cel2 = obj.farenToCelsius(faren);
@@ -16,7 +16,7 @@ public class Application {
 			System.out.println("Faren is NULL -check"+e);
 			System.out.println(e.getMessage());
 		}
-		System.out.println("Celsius value of 45F := " + cel);
+		System.out.println("Celsius value of "+faren+"F : " + cel2);
 	}
 
 }
